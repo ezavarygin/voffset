3b650d3a985ee4ef30bd9777b1d8defe20fdf13b
Evgeny Zavarygin <z5093467@Evgeny-Z.lan>
2016-11-27 13:43:11 +1100

    Initial release

---
+++ functions/functions.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+
+def chi2(off_set,template_func,template_err_func, spec):
+    """
+    The module is to create chi^2.
+    """
+    shift = 1. + off_set/299792.458 # (1+v/c)
+    chi_squared = np.sum((spec.flux[i] - template_func(wave*shift))**2/\
+                             (spec.error[i]**2 + template_err_func(wave*shift)**2)\
+                             for i,wave in enumerate(spec.wave)\
+                             if spec.mask[i])
+    appr_size = np.count_nonzero(spec.mask)
+    return chi_squared/(appr_size-1.0)
+
+
+def detail_wl_array(old_array,resol):
+    """
+    The module is to create a new, with smaller step, wavelength array.
+    
+    (resol-1) - number of subpixels between the old pixels.
+    """
+    new_list = []
+    len_old = len(old_array)
+    resol_rev = 1./resol
+    for i in range(len_old):
+        new_list.append(old_array[i])
+        if i != len_old-1:
+            for j in range(1,resol):
+                new_list.append(old_array[i] + resol_rev*j*(old_array[i+1] - old_array[i]))
+    return np.array(new_list)
+
+
+def off_set_plot(list):
+    """
+    The module is to create a bar chart with distribution of the off-sets.
+    """
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as ticker
+    from matplotlib import rc
+    rc('font', family='Comic Sans MS')
+    rc('axes', labelsize=16)
+#    with open(list) as f:# in case list is path to file voffset_result.dat
+#        data = f.read().split('\n')
+#    data = map(float,[data_i.split(' ')[1] for data_i in data])
+    data = list
+    bin_min = np.min(data)
+    bin_max = np.max(data)
+    bin_step = 0.1
+    if bin_min >= 0:
+        bins = np.arange(bin_step*(bin_min//bin_step - 1), bin_step*(bin_max//bin_step + 2),bin_step)
+    elif bin_max < 0:
+        bins = np.arange(bin_step*(bin_min//bin_step), bin_step*(bin_max//bin_step + 3),bin_step)
+    elif bin_min < 0 and bin_max >= 0:
+        bins = np.arange(bin_step*(bin_min//bin_step), bin_step*(bin_max//bin_step + 2),bin_step)
+    else:
+        print "Something wrong with min/max bins!"
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+    ax.hist(data, bins, histtype='bar', facecolor='#a6cee3',edgecolor = '#1f78b4',alpha=1)# rwidth=0.95
+    ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    ax.set_ylabel('Number of spectra')
+    ax.set_xlabel('Velocity off-set, km/s')
+#    ax.set_xlim([bin_step*(bin_min//bin_step-0.9),bin_step*(bin_max//bin_step + 1.95)])
+#    ax.set_ylim([0.01,4.3])
+    fig.savefig('voffset_dist.pdf',bbox_inches='tight',pad_inches=0)
+    print "................................................................................."
+    print ""
+    print "The file 'voffset_dist.pdf' has been created!"
+    plt.close()
---
+++ functions/spec_parse.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+import numpy as np
+from barak.convolve import convolve_constant_dv
+from astropy.io import fits
+
+
+class Up_parse:
+    """
+    The class is to parse a UVES_popler output file.
+    
+    It provides wavelength, flux, sigma_error, mask (valid pixels) arrays.
+    """
+    def __init__(self, path_to_fits):
+        self.fits_opened = fits.open(path_to_fits)
+        self.file_name = path_to_fits.split('/')[-1]
+        self.wave   = np.array([self.pix2wave(pix) for pix in range(self.fits_opened[0].data[0].size)])
+        self.flux   = self.fits_opened[0].data[0]
+        self.error  = self.fits_opened[0].data[1]
+        self.disp   = self.fits_opened[0].header['UP_DISP']
+        self.mask   = np.array([True if status == 1.0 else False for status in self.fits_opened[0].data[4]])
+#        self.status = self.fits_opened[0].data[4] # clipping status in Up
+        self.fits_opened.close()
+#        self.weight = np.array([1.0 if mask else 1E-10 for mask in self.mask]) # no working in spline?
+        self.length = len(self.flux)
+        
+
+    def pix2wave(self,pixel):
+        """
+        Convert a pixel number into a wavelength.
+
+        It is based on the info from the header.
+        """
+        log_lin = self.fits_opened[0].header['DC-FLAG']
+        if log_lin == 1:
+            wl_0     = 10**self.fits_opened[0].header['CRVAL1']
+            CRPIX1   = self.fits_opened[0].header['CRPIX1']
+            log_disp = self.fits_opened[0].header['CD1_1']
+            w_i=wl_0*10**(((pixel+1)-CRPIX1)*log_disp)
+        else:
+            sys.exit('wavelength scale should be log-linear!')
+        return w_i
+
+    def fix_clip(self):
+        """
+        Makes flux values in clipped pixels to be np.nan.
+        """
+        for i in range(self.length):
+            if not self.mask[i]:
+                self.flux[i] = np.nan
+
+    def fix_clip_err_nan(self):
+        """
+        Makes error values in clipped pixels to be np.nan.
+        """
+        for i in range(self.length):
+            if not self.mask[i]:
+                self.error[i] = np.nan
+
+    def fix_clip_err_med(self):
+        """
+        Makes error values in clipped pixels to be mean.
+        """
+        for i in range(self.length):
+            if not self.mask[i]:
+                self.error[i] = 1.0
+        median = np.median(self.error)
+        for i in range(self.length):
+            if not self.mask[i]:
+                self.error[i] = median
+
+    def convolve(self, res):
+        """
+        Convolves the spectrum using the Barak package.
+        
+        res is a path to the file with Gaussian kernel specified with '--fwhm'.
+        """
+        if res == None:
+            self.flux = convolve_constant_dv(self.wave,self.flux,wa_dv=self.wave,npix=4)
+        else:
+            self.flux = convolve_constant_dv(self.wave,self.flux,vfwhm=self.spec_fwhm_file(res)[self.file_name])
+
+    def spec_fwhm_file(self,res):
+        """
+        Creates a dictionary with "spectrum name : fwhm".
+        
+        It takes fwhms from the res file specified with '--fwhm'.
+        """
+        with open(res,'r') as f:
+            res_split = f.read().split('\n')
+        fwhm = dict()
+        for line in res_split:
+            fwhm[line.split()[0]] = float(line.split()[1])
+        return fwhm
+
+    def get_mask(self,an_spec):
+        """
+        Defines an array of valid pixels.
+        
+        Make a projection of bad pixels in the anchor spectrum onto the one being under analysis.
+        The pixel numbers are in frame of the analysed specrum.
+        The output array therefore takes into account bad pixels of both anchor and analysed spectra.
+        """
+        spec_mask = self.mask
+        pix2pix = self.pix2an_pix(an_spec)
+        an_length = an_spec.length
+        if an_spec.file_name == self.file_name:
+            pass
+        else:
+            for i in range(self.length):
+                if spec_mask[i]:
+                    if pix2pix[i] < 0 or pix2pix[i] >= an_length:
+                        spec_mask[i] = False
+                    else:
+                        if an_spec.mask[pix2pix[i]]:
+                            pass
+                        else:
+                            spec_mask[i] = False
+                else:
+                    pass
+        self.mask = spec_mask
+
+    def pix2an_pix(self,an_spec):
+        """
+        Defines a relation between pixels in an_spec and the analysed spectrum.
+        
+        Defines which pixel numbers in an_spec the pixels in the analysed spectrumn corresponds to.
+        Returns array of len(self.flux) with corresponding pixel indices of the anchor spectrum.
+        """
+        an_wl_0     = 10**an_spec.fits_opened[0].header['CRVAL1']
+        an_CRPIX1   = an_spec.fits_opened[0].header['CRPIX1']
+        an_log_disp = an_spec.fits_opened[0].header['CD1_1']
+        an_index = []
+        for i,wave in enumerate(self.wave):
+            an_index.append(int(round(np.log10(wave/an_wl_0)/an_log_disp + an_CRPIX1 - 1.)))
+        return np.array(an_index)
